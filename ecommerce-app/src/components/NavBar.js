a15822f2cf9dd78bcc01b03568346811e0e6e926
Kiara <kiaraharris168@gmail.com>
2022-04-06 13:42:09 -0400

    Worked on ecommerce app

--- ecommerce-app/src/components/NavBar.js
+++ ecommerce-app/src/components/NavBar.js
@@ -1,5 +1,7 @@
+import { Search, ShoppingCartOutlined } from '@material-ui/icons';
 import React from 'react'
 import styled from 'styled-components'
+import { Badge } from '@material-ui/core'
 
 const Container = styled.div`
     height: 60px;
@@ -7,11 +9,14 @@ const Container = styled.div`
 const Wrapper = styled.div`
     padding: 10px 20px;
     display: flex;
+    align-items: center;
     justify-content: space-between;
 `;
 
 const Left = styled.div`
     flex: 1;
+    display: flex;
+    align-items: center;
 `;
 
 const Language = styled.span`
@@ -19,13 +24,39 @@ const Language = styled.span`
     cursor: pointer;
 `;
 
+const SearchContainer = styled.div`
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`
+
+const Input = styled.input`
+    border: none;
+`
+
 const Center = styled.div`
     flex: 1;
+    text-align: center;
 `;
+
+const Logo = styled.h1`
+    font-weight: bold;
+`
 
 const Right = styled.div`
     flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
 `;
+
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left: 25px;
+`
 
 const NavBar = () => {
   return (
@@ -33,9 +64,23 @@ const NavBar = () => {
         <Wrapper>
             <Left>
                 <Language>EN</Language>
+                <SearchContainer>
+                    <Input />
+                    <Search style = {{color:"grey", fontSize: 16}} />
+                </SearchContainer>
             </Left>
-            <Center>center</Center>
-            <Right>right</Right>
+            <Center>
+                <Logo>Clothing Site</Logo>
+            </Center>
+            <Right>
+                <MenuItem>REGISTER</MenuItem>
+                <MenuItem>SIGN-IN</MenuItem>
+                <MenuItem>
+                    <Badge badgeContent={4} color="primary">
+                        <ShoppingCartOutlined />
+                    </Badge>
+                </MenuItem>
+            </Right>
         </Wrapper>
     </Container>
   )
